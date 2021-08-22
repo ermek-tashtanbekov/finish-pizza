5840207f7c1ebff90d86906f884f1f6f7a56600c
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-08-22 13:17:51 +0600

    created a function accepting your classes

--- src/components/Сatigories/Catigories.jsx
+++ src/components/Сatigories/Catigories.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react"
 import SortPopap from "./SortPopap/SortPopap"
 
 const Catigories = ({ items }) => {
-    const result = items.map((obj, index) => <li key={`${obj } ${index}`}>{obj}</li>)
+    const [visibiliti, SetVisibiliti] = useState(0)
+    const [active, SetActve] = useState(0)
+    
+    function onselectAtem(index) {
+        SetActve(index)
+       SetVisibiliti(null)
+    }
+
+    console.log(onselectAtem)
     return (
         <div className="content__top">
             <div className="categories">
                 <ul>
-                    <li >Все</li>
-                    {result}
+                    <li  >Все</li>
+                    {items.map((obj, index) => <li
+                        key={`${obj} ${index}`}
+                        onClick={() => onselectAtem(index)}
+                        className={active === index ? 'active' : null}
+                    >{obj}</li>)}
                 </ul>
             </div>
             <SortPopap />
