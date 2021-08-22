521db236ba1aa2306e6ea1df101a9a4e057a882a
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-08-22 23:50:27 +0600

    with axios get ingredients

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
 function App() {
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    axios.get('https://610126d34e50960017c29c33.mockapi.io/items')
+      .then((response) => {
+        console.log(response)
+        setItems(response.data)
+      })
+  }, [])
   return (
     <div className="wrapper">
       <Header />
-      <Home />
+      <Home items={items}/>
     </div>
   );
 }
