c3930c373b8f2cddd0db5f89e931ec7549406ea4
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-08-24 13:09:03 +0600

    creater with redux get axios link

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import { useDispatch } from 'react-redux';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import { fetchPizzas } from './store/action/pizzas';
 import {setPizzas} from './store/action/pizzas'
 function App() {
-  
   const dispatch = useDispatch();
+ 
   useEffect(() => {
-    axios.get('https://610126d34e50960017c29c33.mockapi.io/items')
-     .then(({data}) => (
-      //  console.log(data)
-       dispatch(setPizzas(data))
-     ))
-  }, [dispatch])
+    dispatch(fetchPizzas())
+  }, [])
+  
   return (
     <div className="wrapper">
       <Header />
--- src/store/action/pizzas.js
+++ src/store/action/pizzas.js
@@ -1,5 +1,14 @@
+import axios from "axios";
 
 export const setPizzas = (items) => ({
   type: 'SET_PIZZAS',
   payload: items,
 });
+
+
+export const fetchPizzas = () => (dispatch) => {
+  axios.get('https://610126d34e50960017c29c33.mockapi.io/items')
+    .then(({ data }) => (
+      dispatch(setPizzas(data))
+    ))
+}
